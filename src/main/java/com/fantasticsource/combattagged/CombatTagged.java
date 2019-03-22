7a2a7314814da5b30700309069cc869ce0ee5866
Kyle Koder <silvertallon@gmail.com>
2019-03-22 08:04:15 -0700

    1.12.2.009

--- src/main/java/com/fantasticsource/combattagged/CombatTagged.java
+++ src/main/java/com/fantasticsource/combattagged/CombatTagged.java
@@ -31,7 +31,7 @@ public class CombatTagged
 {
     public static final String MODID = "combattagged";
     public static final String NAME = "Combat Tagged!";
-    public static final String VERSION = "1.12.2.008";
+    public static final String VERSION = "1.12.2.009";
     public static final DamageSource smite = new SmiteDamage();
     private static Logger logger;
     private static Map<EntityPlayer, Integer> timers = new HashMap<>();
