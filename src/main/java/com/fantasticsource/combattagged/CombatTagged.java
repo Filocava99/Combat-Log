f1a77ade4a974342d6ba0918c3f158cf5427c4a8
Kyle Koder <silvertallon@gmail.com>
2019-06-02 12:11:21 -0700

    Indev build 1.12.2.009a

--- src/main/java/com/fantasticsource/combattagged/CombatTagged.java
+++ src/main/java/com/fantasticsource/combattagged/CombatTagged.java
@@ -32,7 +32,7 @@ public class CombatTagged
 {
     public static final String MODID = "combattagged";
     public static final String NAME = "Combat Tagged!";
-    public static final String VERSION = "1.12.2.009";
+    public static final String VERSION = "1.12.2.009a";
     public static final DamageSource smite = new SmiteDamage();
     private static Logger logger;
     private static Map<EntityPlayer, Integer> timers = new HashMap<>();
